dd848bded515a0490e8f3d0a3cd528983b523410
SuphawitCE <b4nknaja@hotmail.com>
2022-09-11 13:48:00 +0700

    feat(CORS): set headers to resolve CORS errors issue on clients

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const app = express();
 // app.use(bodyParser.urlencoded()); //  x-www-form-urlencoded Use with form <form>
 app.use(bodyParser.json()); //  application/json
 
+// CORS
+app.use((req, res, next) => {
+  //  Allow origin and can access all clients using wildcard
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  //  Allow request method or can use a wildcard to allow all methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  //  Allow authorization
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Request can now continue and can be handled by our routes
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
