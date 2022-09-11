0e11881113b13d60ac5fb29343b303f2aafc0b94
SuphawitCE <b4nknaja@hotmail.com>
2022-09-11 12:33:46 +0700

    feat(request&response): Sending requests & responses get post create post

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); //  x-www-form-urlencoded Use with form <form>
+app.use(bodyParser.json()); //  application/json
 
 app.use('/feed', feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,35 @@
 exports.getPosts = (req, res, next) => {
-  console.log('get-post-response: ');
+  // req.body will works cause bodyParser.json()
+  console.log('get-post-request: ', req.body);
+
+  const responseData = {
+    posts: [
+      {
+        title: 'First Post',
+        content: 'This is first post'
+      }
+    ]
+  };
+
+  console.log('get-post-response: ', responseData);
+  res.status(200).json(responseData);
+};
+
+// POST method
+exports.createPost = (req, res, next) => {
+  // req.body will works cause bodyParser.json()
+  console.log('create-post-request: ', req.body);
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  // Create post in DB
+  const responseData = {
+    message: 'Post created successfully',
+    post: { id: new Date().toISOString(), title, content }
+  };
+
+  console.log('create-post-response: ', responseData);
+
+  res.status(201).json(responseData);
 };
