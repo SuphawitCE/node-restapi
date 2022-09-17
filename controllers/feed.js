3be74dac40d9b6bf4530624bfa029e6c0fbf4814
SuphawitCE <b4nknaja@hotmail.com>
2022-09-17 21:55:45 +0700

    refactor(async): fetch to async/await function

--- controllers/feed.js
+++ controllers/feed.js
@@ -26,45 +26,44 @@ exports.getPosts = (req, res, next) => {
 };
 
 // POST method
-exports.createPost = (req, res, next) => {
-  // Validate request
-  const errors = validationResult(req);
+exports.createPost = async (req, res, next) => {
+  try {
+    // Validate request
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation failed, entered data is incorrect.',
-      errors: errors.array()
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array()
+      });
+    }
+
+    // req.body will works cause bodyParser.json()
+    console.log('create-post-request: ', req.body);
+
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const post = new Post({
+      title,
+      content,
+      imageUrl: 'images/fatcat1.jpeg',
+      creator: { name: 'Bank' }
     });
+
+    // Storing posts in the MongoDB
+    const postResult = await post.save();
+
+    const responseData = {
+      message: 'Post created successfully',
+      post: postResult
+    };
+
+    console.log('post-result: ', postResult);
+
+    // Response created post successfully to client-side
+    res.status(201).json(responseData);
+  } catch (error) {
+    console.log('create-post-error:', error);
   }
-
-  // req.body will works cause bodyParser.json()
-  console.log('create-post-request: ', req.body);
-
-  const title = req.body.title;
-  const content = req.body.content;
-
-  const post = new Post({
-    title,
-    content,
-    imageUrl: 'images/fatcat1.jpeg',
-    creator: { name: 'Bank' }
-  });
-
-  post
-    .save()
-    .then((result) => {
-      console.log('post-result: ', result);
-      // Create post in DB
-      const responseData = {
-        message: 'Post created successfully',
-        post: result
-      };
-
-      console.log('create-post-response: ', responseData);
-
-      res.status(201).json(responseData);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
 };
