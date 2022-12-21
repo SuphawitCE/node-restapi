38b710290af0822ab95ee6b760a6da5e00ba632b
SuphawitCE <b4nknaja@hotmail.com>
2022-12-21 12:37:52 +0700

    test: Cleaning up and before after hooks

--- test/controllers/auth.test.js
+++ test/controllers/auth.test.js
@@ -8,7 +8,45 @@ const AuthController = require('../../controllers/auth');
 
 const next = () => {};
 
-describe('Authentication Controller - Login', () => {
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+const collectionName = 'test-messages'; //  MongoDB collection name
+const dbURI = `mongodb+srv://${username}:${password}@cluster0.ypnh4.mongodb.net/${collectionName}`; // MongoDB connection URI
+
+const payload = {
+  email: 'qa@test.com',
+  password: 'qatest123*',
+  name: 'QA',
+  posts: [],
+  _id: '5c0f66b979af55031b34728a'
+};
+
+describe('Authentication Controller', () => {
+  // Create user account before run all tests
+  before((done) => {
+    mongoose
+      .connect(dbURI)
+      .then((result) => {
+        const user = new User(payload);
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  // Delete user account after run all tests
+  after((done) => {
+    User.deleteMany(payload)
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
   it('Should throw an error with status code 500 if database connection failure', async () => {
     sinon.stub(User, 'findOne');
     User.findOne.throws();
@@ -20,7 +58,7 @@ describe('Authentication Controller - Login', () => {
       }
     };
 
-    const result = await AuthController.login(req, {}, next);
+    // const result = await AuthController.login(req, {}, next);
 
     // expect(result).to.be.an('error');
     // expect(result).to.have.property('statusCode', 500);
@@ -35,47 +73,25 @@ describe('Authentication Controller - Login', () => {
   });
 
   it('Should send a response with a valid user status for an existing user', (done) => {
-    const username = process.env.MONGO_USERNAME;
-    const password = process.env.MONGO_PASSWORD;
+    const req = { userId: payload._id };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      json: function (data) {
+        this.userStatus = data.status;
+        return null;
+      },
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      }
+    };
 
-    const collectionName = 'test-messages'; //  MongoDB collection name
-    const dbURI = `mongodb+srv://${username}:${password}@cluster0.ypnh4.mongodb.net/${collectionName}`; // MongoDB connection URI
+    AuthController.getUserStatus(req, res, next).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(req.userStatus).to.be.equal('New user');
 
-    mongoose
-      .connect(dbURI)
-      .then((result) => {
-        const payload = {
-          email: 'qa@test.com',
-          password: 'qatest123*',
-          name: 'QA',
-          posts: [],
-          _id: 'mock'
-        };
-        const user = new User(payload);
-        return user.save();
-      })
-      .then(() => {
-        const req = { userId: payload._id };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          json: function (data) {
-            this.userStatus = data.status;
-            return null;
-          },
-          status: function (code) {
-            this.statusCode = code;
-            return this;
-          }
-        };
-        AuthController.getUserStatus(req, res, next).then(() => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(req.userStatus).to.be.equal('New user');
-          done();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      done();
+    });
   });
 });
